077eaddcb6e050d8d605a9a5e37b6eb6e9de4e1d
Alex <83555703+3xAlex@users.noreply.github.com>
2023-11-09 20:52:18 +0200

    Add files via upload

---
+++ components/index.jsx
@@ -0,0 +1,11 @@
+import Nav from "./nav";
+import Hero from "./Hero";
+import Test from "./Test";
+import Split from "./Split";
+import Split2 from "./Split2";
+import Banner from "./Banner";
+import Products from "./Products";
+import Footer from "./Footer";
+import Carousel from "./Carousel";
+
+export {Nav, Hero, Test, Split, Carousel, Split2, Banner, Products, Footer}
---
+++ components/loader/loader.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const Loader = () => {
+  return (
+    <div className='w-full h-screen mx-auto bg-[#1a1919] z-10 flex justify-center items-center'>
+     <motion.div
+        className="w-[350px] sm:w-[450px]">
+          <img src="gif.gif" className="w-full h-full" />
+        </motion.div>
+    </div>
+  )
+}
+
+export default Loader
